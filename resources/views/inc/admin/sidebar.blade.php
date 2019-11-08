e1d6648c80b962c67dae5135de2ecabc6aec6795
Jerald Dela Cruz <jeralddelacruz004@gmail.com>
2019-11-08 12:55:04 +0800

    sidebar logo link updated

--- resources/views/inc/admin/sidebar.blade.php
+++ resources/views/inc/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/admin/dashboard') }}" class="brand-link">
-        <img src="{!! asset('theme/dist/img/AdminLTELogo.png') !!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="{!! asset('/bower_components/admin-lte/dist/img/AdminLTELogo.png') !!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">{{ config('app.name', 'Mini-CRM') }}</span>
     </a>
